cc805989734d7e9deb0b0247c8ebbfcd51fd1a2f
emidec <e.decristofaro@ucl.ac.uk>
2021-12-15 19:38:49 +0000

    citedBy

--- citedBy.py
+++ citedBy.py
@@ -18,37 +18,33 @@ seed = scholarly.fill(first_result)
 #scholarly.pprint(paper)
 print("Successfully retrieved paper\n\n")
 
+
+columns = ['cites', 'title', 'author', 'year', 'cites_per_year', 'pub_url', 'publisher', 'cites_id', 'citedby_url']
+papers = pd.DataFrame(columns=columns)
+
+pubs = []
 #citations = [citation['bib']['title'] for citation in scholarly.citedby(paper)]
-citations = scholarly.citedby(seed)
+for cit in scholarly.citedby(seed):
+	pub = scholarly.fill(cit)
+	pubs.append(pub)
+#	print(pub['bib'].get('title'))
 
-#columns = ['cites', 'title', 'author', 'year', 'cites_per_year', 'pub_url', 'publisher', 'cites_id', 'citedby_url']
+for i in range(len(pubs)):
+	print("Processing pub " + str(i+1) + "/" + str(len(pubs)))
+	pub = pubs[i]
+	papers = papers.append(pd.Series([
+	pub.get('num_citations'),
+	pub['bib'].get('title'),
+	pub['bib'].get('author'),
+	pub['bib'].get('pub_year'),
+	pub.get('cites_per_year'),
+	pub.get('pub_url'),
+	pub['bib'].get('publisher'),
+	pub.get('cites_id'),
+	pub.get('citedby_url')
+	], index=papers.columns), ignore_index=True)
 
-#papers = pd.DataFrame(columns=columns)
-
-#num_pubs = len(citations['publications'])
-
-#print(seed + " has " + str(num_pubs) + " citations on Scholar.")
-
-# #print(author['publications'][0]['bib'].get('title'))
-
-# for i in range(len(author['publications'])):
-# 	print("Processing pub " + str(i+1) + "/" + str(num_pubs))
-# 	tmp = author['publications'][i]
-# 	pub = scholarly.fill(tmp)
-# 	profile = profile.append(pd.Series([
-# 	pub.get('num_citations'),
-# 	pub['bib'].get('title'),
-# 	pub['bib'].get('author'),
-# 	pub['bib'].get('pub_year'),
-# 	pub.get('cites_per_year'),
-# 	pub.get('pub_url'),
-# 	pub['bib'].get('publisher'),
-# 	pub.get('cites_id'),
-# 	pub.get('citedby_url')
-# 	], index=profile.columns), ignore_index=True)
-# 	print(profile.loc[i])
-
-# today = datetime.today().strftime('%Y%m%d')
-# filename = author_name.replace(" ", "") + "_complete_" + today + ".csv"
-# print("Done. Saving to file... " + filename)
-# profile.to_csv(filename,index=False)
+today = datetime.today().strftime('%Y%m%d')
+filename = paper_title.replace(" ", "") + "_complete_" + today + ".csv"
+print("Done. Saving to file... " + filename)
+profile.to_csv(filename,index=False)
