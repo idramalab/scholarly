517b3fd822c6b6291a7941bc44fa66ca259545ed
emidec <e.decristofaro@ucl.ac.uk>
2021-12-16 03:52:06 +0000

    proxy

--- citedBy.py
+++ citedBy.py
@@ -2,6 +2,10 @@ from scholarly import scholarly
 import pandas as pd
 import sys
 from datetime import datetime
+import time
+import random
+from scholarly import ProxyGenerator
+
 
 if len(sys.argv) != 2:
 	print("Usage: citedBy.py \"Paper Title\"")
@@ -19,15 +23,25 @@ seed = scholarly.fill(first_result)
 print("Successfully retrieved paper\n\n")
 
 
-columns = ['cites', 'title', 'author', 'year', 'cites_per_year', 'pub_url', 'publisher', 'cites_id', 'citedby_url']
+columns = ['cites', 'title', 'author', 'year', 'pub_url', 'publisher']
 papers = pd.DataFrame(columns=columns)
 
 pubs = []
 #citations = [citation['bib']['title'] for citation in scholarly.citedby(paper)]
+
+pg = ProxyGenerator()
+success = pg.ScraperAPI("2f390977f8a7b3df40d092926587b667")
+scholarly.use_proxy(pg)
+print("Now using Proxy")
+
 for cit in scholarly.citedby(seed):
 	print(cit['bib']['title'])
-	pub = scholarly.fill(cit)
-	pubs.append(pub)
+	#pub = scholarly.fill(cit)
+	#print("Filled")
+	pubs.append(cit)
+	#s = random.uniform(5,15)
+	#print("Sleeping for " + s + " seconds")
+	#time.sleep(random.uniform(5,15))
 #	print(pub['bib'].get('title'))
 
 print("Done with citedby retrieval. Now saving shit.")
@@ -40,14 +54,11 @@ for i in range(len(pubs)):
 	pub['bib'].get('title'),
 	pub['bib'].get('author'),
 	pub['bib'].get('pub_year'),
-	pub.get('cites_per_year'),
 	pub.get('pub_url'),
-	pub['bib'].get('publisher'),
-	pub.get('cites_id'),
-	pub.get('citedby_url')
+	pub['bib'].get('publisher')
 	], index=papers.columns), ignore_index=True)
 
 today = datetime.today().strftime('%Y%m%d')
 filename = paper_title.replace(" ", "") + "_complete_" + today + ".csv"
 print("Done. Saving to file... " + filename)
-profile.to_csv(filename,index=False)
+papers.to_csv(filename,index=False)
